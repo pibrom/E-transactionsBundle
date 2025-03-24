31efd631885d86f407da30d31ada9d47b3ad09f3
Romaric PIBOLLEAU <contact@rp2i.net>
2025-03-24 14:33:07 +0100

    Update to make it compatible with Symfony 7+
    Update the codebase to be compatible with Symfony 7+.
    
    * **DependencyInjection/Configuration.php**
      - Update `getConfigTreeBuilder` method to use the new `TreeBuilder` constructor and `getRootNode` method.
    
    * **Resources/config/services.yml**
      - Update service definitions to use `%kernel.project_dir%` instead of `%kernel.root_dir%`.
    
    * **Twig/TwigExtension.php**
      - Replace deprecated `Twig_Extension` and `Twig_SimpleFunction` with `AbstractExtension` and `TwigFunction`.
      - Update method signatures to use `Environment` instead of `Twig_Environment`.
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/Snowbaha/E-transactionsBundle?shareId=XXXX-XXXX-XXXX-XXXX).

--- Twig/TwigExtension.php
+++ Twig/TwigExtension.php
@@ -2,42 +2,41 @@
 
 namespace Snowbaha\EtransactionsBundle\Twig;
 
-use \Twig_Extension;
-use \Twig_SimpleFunction;
-use \Twig_Environment;
-
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+use Twig\Environment;
 
 /**
  * Class TwigExtension
  * @package Snowbaha\EtransactionsBundle\Twig
  */
-class TwigExtension extends Twig_Extension
+class TwigExtension extends AbstractExtension
 {
     /**
      * @return array
      */
     public function getFunctions()
     {
-        return array(
-            new Twig_SimpleFunction(
+        return [
+            new TwigFunction(
                 'paiementForm',
-                array($this, 'paiementForm'),
-                array(
-                    'is_safe' => array('html'),
+                [$this, 'paiementForm'],
+                [
+                    'is_safe' => ['html'],
                     'needs_environment' => true
-                )
+                ]
             ),
-        );
+        ];
     }
 
     /**
-     * @param Twig_Environment $twig  // allow to use the render (param in getFunctions() are very important!)
+     * @param Environment $twig  // allow to use the render (param in getFunctions() are very important!)
      * @param $fields
      * @return mixed
      */
-    public function paiementForm(Twig_Environment $twig, $fields)
+    public function paiementForm(Environment $twig, $fields)
     {
-        $form_html = $twig->render('SnowbahaEtransactionsBundle:Etransactions:form.html.twig', array('fields' => $fields));
+        $form_html = $twig->render('SnowbahaEtransactionsBundle:Etransactions:form.html.twig', ['fields' => $fields]);
 
         return $form_html;
     }
